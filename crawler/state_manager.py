169ced6b7dc22dbc5e990f83fdf0a536ab42abea
dinosaursrarr <105217023+dinosaursrarr@users.noreply.github.com>
2023-02-20 13:51:34 +0000

    Small fix

--- crawler/state_manager.py
+++ crawler/state_manager.py
@@ -14,24 +14,25 @@ class StateManager(abc.ABC):
         Lets us know when all the work is finished.
         :return: True when there is nothing left to crawl, False otherwise.
         """
-        raise NotImplementedError('Cannot call is_finished on abstract base class Checkpointer')
+        raise NotImplementedError('Cannot call is_finished on abstract base class StateManager')
 
     def enqueue(self, url: str) -> None:
         """
         Adds url to the list of URLs to be crawled.
         :param url: URL to be added to the crawl queue.
         """
-        raise NotImplementedError('Cannot call enqueue on abstract base class Checkpointer')
+        raise NotImplementedError('Cannot call enqueue on abstract base class StateManager')
 
     def pop_next(self) -> str:
         """
         Retrieves the next URL that should be visited.
         :return: The next URL to be crawled.
         """
+        raise NotImplementedError('Cannot call pop_next on abstract base class StateManager')
 
     def mark_completed(self, url: str) -> None:
         """
         Updates crawl state so that we know `url` has been processed.
         :param url: URL to be marked completed.
         """
-        raise NotImplementedError('Cannot call mark_completed on abstract base class Checkpointer')
+        raise NotImplementedError('Cannot call mark_completed on abstract base class StateManager')
